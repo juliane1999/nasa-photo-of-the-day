8c673ae897c1605397e033ebc8a10ecf708a784d
juliane1999 <lastjuliane@gmail.com>
2021-07-15 23:58:34 -0700

    final commit

--- src/Components/Explanation.js
+++ src/Components/Explanation.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function Explanation (props) {
     
     const {explanation} = props;
+    const StyleExplanation = styled.div`
+    padding: 5rem
+    `
 
     return (
         <div className = 'nasa-explanation'>
+            <StyleExplanation explanation={explanation}>
             <span className = 'explanation'>{explanation}</span>
+            </StyleExplanation>
         </div>
     )
 
